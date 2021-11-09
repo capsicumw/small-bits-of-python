6d0392ebbd08d87a5626f7f988d56a52239d3960
capsicumw <30839415+capsicumw@users.noreply.github.com>
2021-11-09 15:36:55 -0800

    Add files via upload

---
+++ Tree.py
@@ -0,0 +1,38 @@
+import sys
+
+
+iSize = input( "Enter size of tree[recommend 6 to 64]:" )
+
+#check input is rational
+count = 0
+while count < 2 :
+    if float( iSize ) < 0 :
+        count = count + 1
+        #print (count)
+        if count == 2 :
+            print( "\n¡Why you no Read?!\n")
+            sys.exit( "I have better things to do. Go away now!" )
+        print( "\nHow can a tree have negative size?")
+        iSize = input( "Enter size of tree[recommend 6 to 64]:" )
+    else:
+        break
+
+
+#Calculate stem before any rounding or truncation
+stemLen = int( float( iSize ) / 4 )
+stemWidth = int( float( iSize ) / 6 )
+
+#truncate and cast input string to integer for looping and character counts.
+size = int( float( iSize ) )
+
+#print top of tree
+for cv in range( size ):
+    y = cv * "A"
+    x = int((size - 1 - cv)/2) * " "
+    print( x + y )
+
+#print stem of tree
+for cv in range( stemLen ):
+    y = "|" * stemWidth
+    x = " " * int( ( size * 0.5 - stemWidth * 0.5) )
+    print( x + y )
